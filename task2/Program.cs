7a925174fc7acd220b7d80362ce41888b3669450
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-11 19:19:43 +0300

    added program cs

---
+++ task2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите номер ");
+string answer = Console.ReadLine();
+Console.WriteLine("Какой день недели? ");
+
+if(answer == "1")
+{
+    Console.WriteLine("Понедельник - новая жизнь! ");
+}
+if(answer == "2")
+{
+    Console.WriteLine("Вторник - почти среда! ");
+}
+if(answer == "3")
+{
+    Console.WriteLine("Среда - маленькая пятница! ");
+}
+if(answer == "4")
+{
+    Console.WriteLine("Четверг - рыбный день! ");
+}
+if(answer == "5")
+{
+    Console.WriteLine("Ура, пятница!!!  ");
+}
+if(answer == "6")
+{
+    Console.WriteLine("Суббота - выходнооой! ");
+}
+if(answer == "7")
+{
+    Console.WriteLine("Воскресенье - завтра понедельник... ");
+}
