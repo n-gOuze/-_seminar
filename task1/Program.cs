36ed81341a6644dbf230186f460a5c5fde880c9b
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-11 19:07:16 +0300

    added program cs

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число ");
+string answerA = Console.ReadLine();
+int numberA = Convert.ToInt32(answerA);
+Console.WriteLine("Введите второе число ");
+string answerB = Console.ReadLine();
+int numberB = Convert.ToInt32(answerB);
+Console.WriteLine("Является ли первое число квадратом второго? ");
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Да ");
+    }
+else
+{
+  Console.WriteLine("Нет ");
+}
+
