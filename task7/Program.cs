700ee05b9855508d69f9576911d8e3559b0282a0
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-12 12:24:09 +0300

    added task7 solution

---
+++ task7/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Является ли число четным? ");
+
+int result = number % 2;
+if(result == 0)
+{
+    Console.WriteLine("Да ");
+}
+else
+{
+    Console.WriteLine("Нет ");
+}
+
+
