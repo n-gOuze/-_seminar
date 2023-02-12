fb7e2d87569a90dd1b82104c415e1c74ded191a1
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-12 11:47:01 +0300

    added task5 solution

---
+++ task5/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
+else if(numberA < numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine("Числа равны ");
+}
