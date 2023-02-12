cd1a5f3dc09df5939a1c1b51e183077dfdaa7677
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-12 11:28:43 +0300

    added task3 solution

---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Целые числа в промежутке от -N до N ");
+for(int i = -n; i <= n; i++)
+{
+    Console.WriteLine(i);
+}
