a6d7d2ac1b93cee796a4de72743c2541c7355124
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-12 12:50:08 +0300

    added task8 solution

---
+++ task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = -n; i <= n; i++)
+{
+    int result = i % 2;
+    if(result == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
