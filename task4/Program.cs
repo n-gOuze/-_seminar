7e202c48ab8e3d3efd442444e856e0ae435cc5c4
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-12 11:34:25 +0300

    added task4 solution

---
+++ task4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Определим последнюю цифру этого числа ");
+int result = number % 10;
+Console.WriteLine(result);
