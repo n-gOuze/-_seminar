a72e98cc67c972c163ff2fb0bb37e401c13c2915
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-16 15:51:28 +0300

    added task10 solution

---
+++ task10/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = Random.Shared.Next(100, 1000);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+int firstDigit = number / 100;
+// Console.WriteLine($"{firstDigit}{lastDigit}");
+int result = firstDigit*10 + lastDigit;
+Console.WriteLine(result);
