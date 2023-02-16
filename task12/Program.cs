a18fde6e773d7053428472baeafa9d5e4ba88788
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-16 17:00:59 +0300

    added task12 solution

---
+++ task12/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int resultA = number % 7; 
+int resultB = number % 23;
+if(resultA == 0)
+{
+    if(resultB == 0)
+    {
+        Console.WriteLine("да ");
+    }
+}
+else if(resultB == 0)
+{
+    {
+        Console.WriteLine("нет ");
+    }
+}
+else
+{
+    Console.WriteLine("нет ");
+}
