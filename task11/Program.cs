588ca58cbd84e2323dbe6616ca27eca8a9797114
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-16 16:09:08 +0300

    added task11 solution

---
+++ task11/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberB % numberA;
+if(result == 0)
+{
+    Console.WriteLine("Кратно ");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток {result}");
+}
