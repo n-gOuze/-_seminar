1c469fccdb8777a58ea918cc54100eb504a1c6fc
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-16 15:35:06 +0300

    added task9 solution

---
+++ task9/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = Random.Shared.Next(10, 100);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+//Console.WriteLine(lastDigit);
+int firstDigit = number / 10;
+//Console.WriteLine(firstDigit);
+if(firstDigit > lastDigit)
+{
+   // Console.WriteLine("max");
+   // Console.WriteLine(firstDigit);
+   // Console.WriteLine("min");
+   // Console.WriteLine(lastDigit);
+   // Console.WriteLine("max" + firstDigit + "min" + lastDigit);
+   Console.WriteLine($"max {firstDigit}, min {lastDigit}");
+}
+else
+{
+   // Console.WriteLine(lastDigit);
+   Console.WriteLine($"max {lastDigit}, min {firstDigit}");
+}
