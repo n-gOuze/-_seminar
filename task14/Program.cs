23713e1ea06867240e43fed64f61f5a4fc5a320f
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-19 14:37:16 +0300

    added error version

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,5 +1,12 @@
 ﻿Console.WriteLine("Введите трехзначное число ");
 int number = Convert.ToInt32(Console.ReadLine());
-int firstDigit = number / 100;
-int secondDigit = (number - firstDigit*100) / 10;
-Console.WriteLine(secondDigit);
+if(number >= 100 && number < 1000)
+{
+    int firstDigit = number / 100;
+    int secondDigit = (number - firstDigit*100) / 10;
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("error");
+}
--- task15/Program.cs
+++ task15/Program.cs
@@ -1,12 +1,3 @@
 ﻿Console.WriteLine("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
-void FillArray(int [] number)
-{
-    int length = number.Length;
-    int index = 0;
-}
-while (index < 3)
-{
-    number[index] = index++;
-}
-int position = IndexOf(array, 3);
+
