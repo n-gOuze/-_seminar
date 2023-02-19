eb0902f0dc4d512ae8bf6faf902c6d921d388885
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-19 12:28:35 +0300

    added task16 solution

---
+++ task16/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер дня недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 6)
+{
+    Console.WriteLine("да ");
+}
+else if(number == 7)
+{
+    Console.WriteLine("да ");
+}
+else
+{
+    Console.WriteLine("нет ");
+}
