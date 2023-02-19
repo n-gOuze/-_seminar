a61e84cb0394342c92cca7b60ea02da5827996d0
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-19 14:20:52 +0300

    added || version

--- task13/Program.cs
+++ task13/Program.cs
@@ -2,11 +2,7 @@
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int numberB = Convert.ToInt32(Console.ReadLine());
-if(numberA == numberB*numberB)
-{
-    Console.WriteLine("да ");
-}
-else if(numberB == numberA*numberA)
+if(numberA == numberB*numberB || numberB == numberA*numberA)
 {
     Console.WriteLine("да ");
 }
