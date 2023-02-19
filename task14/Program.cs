31864c0d270ddb1acf28f78b8933689f28ccfe7d
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-19 12:07:49 +0300

    added task14 solution

---
+++ task14/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 100;
+int secondDigit = (number - firstDigit*100) / 10;
+Console.WriteLine(secondDigit);
