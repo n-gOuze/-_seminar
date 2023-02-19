3e80ea65bd062a85e41c449a349703fccc30e270
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-19 15:16:54 +0300

    added task15 solution

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,3 +1,16 @@
 ﻿Console.WriteLine("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
-
+Console.WriteLine("Определим третью цифру этого числа ");
+if(number < 100)
+{
+    Console.WriteLine("третьей цифры нет ");
+}
+else
+{
+    while(number > 999)
+    {
+        int count = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine(result);
+}
