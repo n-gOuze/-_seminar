0f3ea45ccc29519a9f07d8350dd0beb24a1d1bd7
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-19 11:24:10 +0300

    added task13 solution

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA == numberB*numberB)
+{
+    Console.WriteLine("да ");
+}
+else if(numberB == numberA*numberA)
+{
+    Console.WriteLine("да ");
+}
+else
+{
+    Console.WriteLine("нет ");
+}
