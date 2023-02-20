9cd32f4531efdb7b1bf851c1ace5db7829272279
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-20 13:50:12 +0300

    added task17 solution

---
+++ task17/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите координаты точки по X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки по Y");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находится в первой координатной четверти");
+}
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("Точка находится во второй координатной четверти");
+}
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в третьей координатной четверти");
+}
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в четвертой координатной четверти");
+}
+else
+{
+    Console.WriteLine("Невозможно определить координатную четверть");
+}
