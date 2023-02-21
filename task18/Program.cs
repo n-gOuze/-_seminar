7623bfb4e902db54fe394afb8f0ef2eefc3dde2d
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-21 14:22:14 +0300

    added task18 solution

---
+++ task18/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("x > 0 && y > 0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("x < 0 && y > 0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("x < 0 && y < 0");
+}
+else if (number == 4)
+{
+    Console.WriteLine("x > 0 && y < 0");
+}
+else
+{
+    Console.WriteLine("Неверно введен номер четверти");
+}
