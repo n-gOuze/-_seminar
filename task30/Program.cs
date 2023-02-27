655cb1c3147890aa137a6f9f98194ae393afe223
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-27 14:38:13 +0300

    added task30 solution

---
+++ task30/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Сделала для чисел от 1 до 99, чтобы было очевидно, что цифр 8
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine(collection[index]);
+        index++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
