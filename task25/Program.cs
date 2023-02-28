b045a920f7c300fd84e9d706e44885265d7a30e4
Guseva Natalia <natalia.gyseva@gmail.com>
2023-02-28 09:05:07 +0300

    added task25 solution

--- task25/Program.cs
+++ task25/Program.cs
@@ -2,3 +2,23 @@
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountOfDigits(int number)
+{
+    int count;
+    for(count = 0; number !=0; count++)
+    {
+        number /= 10; //number = number / 10
+    }
+    return count;
+}
+
+int usernumber = ReadNumber("Введите значение");
+int result = CountOfDigits(usernumber);
+Console.WriteLine(result);
