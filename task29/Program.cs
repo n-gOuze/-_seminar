209f9558f6fea1ee7efb10c77f9a024ddb9ba52e
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-02 15:26:22 +0300

    added task28 cycle solution

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,1 +1,22 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountOfDigits(int number)
+{
+    int count;
+    for(count = 0; number !=0; count++)
+    {
+        number /= 10; //number = number / 10
+    }
+    return count;
+}
+
+int usernumber = ReadNumber("Введите число");
+int result = CountOfDigits(usernumber);
+Console.WriteLine(result);
