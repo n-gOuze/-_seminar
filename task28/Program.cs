422987247d53f603e03cc1dfb114b144c314a2e3
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-03 09:46:22 +0300

    added new task28 solution

---
+++ task28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = ReadNumber("Введите число А");
+int b = ReadNumber("Введите число B");
+
+// double res = Math.Pow(a, b);
+
+int CountOfA(int a, int b)
+{
+    int result = 1;
+    for(int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int power = CountOfA(a, b);
+Console.WriteLine(power);
