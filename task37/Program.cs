e4af44f119e921862ac710ad1bf903e1a901d915
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-04 18:53:22 +0300

    added task37 solution

---
+++ task37/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 100);
+    }
+}
+
+int SumOfElements (int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = new int[4];
+FillArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+int res = SumOfElements(array);
+Console.WriteLine(res);
