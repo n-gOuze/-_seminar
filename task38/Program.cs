8266a0370108fb3c5a284f29f7caabd3b616eae6
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-07 17:13:49 +0300

    added info about double array

--- task38/Program.cs
+++ task38/Program.cs
@@ -31,3 +31,13 @@ Console.WriteLine($"[{string.Join(", ", array)}]");
 int res = MaxMinNum(array);
 Console.WriteLine(res);
 
+// Для вывода числа Double
+// int numberInt = Random.Shared.Next(1, 10);
+// double numberDouble = Random.Shared.NextDouble(); // [0.0, 1.0)
+// double numberDouble = Random.Shared.NextDouble() * 10; // [0.0, 10.0)
+// double numberDouble = Random.Shared.NextDouble() * 100 - 20; // [-20.0, 80.0)
+// Console.WriteLine($"{numberDouble:f2}); //чтобы показать определенное количество знаков после , (2)
+
+// Для вывода массива с числами Double
+// double[] array = new double[] {2.35345345, 1.392391392, 234.23423};
+// Console.WriteLine($"{string.Join(", ", array.Select(x => Math.Round(x, 2)))}");
