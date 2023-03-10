43d1eb2abd0e0f661845148befe65729ee10a3ac
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-10 11:55:57 +0300

    added task44 teacher solution

--- task44/Program.cs
+++ task44/Program.cs
@@ -44,3 +44,21 @@ Console.WriteLine($"Было введено {result} положительных 
 // Пользователь вводит с клавиатуры неопределенное количество чисел (while (true)). 
 // Посчитайте, сколько чисел больше 0 ввёл пользователь. 
 // Ввод чисел останавливается при помощи ввода слова "stop"
+
+int count = 0;
+string stop = "stop";
+while (true)
+{
+    string userAnswer = Console.ReadLine()!;
+    if (userAnswer.ToLower() == stop)
+    {
+        break;
+    }
+
+    int number = Convert.ToInt32(userAnswer);
+    if (number > 0)
+    {
+        count++;
+    }
+    Console.WriteLine(count);
+}
