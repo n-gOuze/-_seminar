bfcfb32b6209cb2a353b8fafd0355c136be7be60
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-11 21:57:02 +0300

    added task55 solution

---
+++ task55/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// Массив
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+void CreateMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CountNumbers (int[,] matrix)
+{
+    int[] count = new int[10];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int index = matrix[i, j];
+            count[index]++;
+        }
+    }
+
+    for (int i = 0; i < count.Length; i++)
+    {
+        Console.WriteLine($"{i} повторяется {count[i]} раз");
+    }
+}
+
+int[,] matrix = new int[3, 3];
+CreateMatrix(matrix);
+PrintMatrix(matrix);
+CountNumbers(matrix);
+
