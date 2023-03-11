43b88acaad0eb617b21b14a1358b144a435e20e6
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-11 16:25:55 +0300

    added task51 solution

---
+++ task51/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int m = ReadNumber("Введите индекс по номеру строки");
+int n = ReadNumber("Введите индекс по номеру столбца");
+
+void CreateMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int CheckNumber (int[,] matrix)
+{
+    int res = 0;
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                res = matrix[m, n];
+            }
+    }
+    return res;
+}
+
+int[,] matrix = new int[4, 5];
+CreateMatrix(matrix);
+PrintMatrix(matrix);
+int result = CheckNumber(matrix);
+Console.WriteLine(result);
+
