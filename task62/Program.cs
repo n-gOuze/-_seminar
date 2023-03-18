4eb066bc4f7edf426d487440931e4a3f19a9cb3e
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-18 12:17:07 +0300

    added task62 solution

---
+++ task62/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вычислить факториал введенного числа N
+// 4 -> 24
+// 5 -> 120
+
+uint number = 5;
+
+uint Factorial(uint n) // uint - целое неотрицательное число
+{
+    if (n < 0) throw new Exception ($"Невозможно вычислить факториал числа {n}");
+    if (n == 0) return 1;
+    if (n == 1) return 1;
+    uint result =n * Factorial(n - 1);
+    return result;
+}
+
+Console.WriteLine(Factorial(number));
