53fc22fe9f8b9e297dca45119b9bb09e8582a390
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-18 12:57:43 +0300

    added task64 solution

---
+++ task64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 65: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+int numberM = 1;
+int numberN = 5;
+
+void Numbers (int m, int n)
+{
+    if (n < m) return;
+    Numbers(m, n - 1);
+    Console.Write($"{n}" + " ");
+}
+
+Numbers(numberM, numberN);
