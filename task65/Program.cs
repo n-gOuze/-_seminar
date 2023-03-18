42e18e8a73de72d767a5b89efb93bc3dfaffaa67
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-18 13:06:20 +0300

    added task65 solution

---
+++ task65/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int number = 453;
+
+int Sum (int n)
+{
+    if (n == 0) return 0;
+    int result = n % 10 + Sum(n / 10);
+    return result;
+}
+
+Console.WriteLine(Sum(number));
