e8b82fa019170f194c374261546257cdd93e3f0d
Guseva Natalia <natalia.gyseva@gmail.com>
2023-03-18 13:32:54 +0300

    added task67 solution

---
+++ task67/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напечатать двоичное представление числа 13
+
+int number = 13;
+
+void PrintBinaryView (int n)
+{
+    if (n == 0) return;
+    PrintBinaryView(n/ 2);
+    Console.Write(n % 2);
+}
+
+PrintBinaryView(number);
+
+
